a549bf5dcf014f637ae43dbb6a77bfc2b41f95c0
Đạt Tiến <tien-dat@system-exe.com.vn>
2024-07-15 22:12:23 +0700

    create app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan"); // Hiển thị thông tin request như số port màu gì, thời gian gửi 1 request
+const app = express();
+
+//init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+//init db
+
+//init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello Fantipjs";
+
+  return res.status(200).json({
+    message: "Welcome Fantipjs",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+//handling error
+
+module.exports = app;

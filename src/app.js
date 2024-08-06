99e855f3dd16709353e14990b1d06a643730a212
anh-tuant <anh-tuant@system-exe.com>
2024-08-06 19:38:32 +0700

    setup-env

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const { default: helmet } = require("helmet");
 const compression = require("compression");
 const morgan = require("morgan"); // Hiển thị thông tin request như số port màu gì, thời gian gửi 1 request
 const app = express();
-
-console.log(`Process:`, process.env);
 
 //init middlewares
 app.use(morgan("dev"));
@@ -13,17 +12,10 @@ app.use(compression());
 
 //init db
 require("./dbs/init.mongodb");
-const { checkOverloadedConnections } = require("./helpers/check.connect");
-checkOverloadedConnections();
+// const { checkOverloadedConnections } = require("./helpers/check.connect");
+// checkOverloadedConnections();
 //init routes
-app.get("/", (req, res, next) => {
-  //   const strCompress = "Hello Fantipjs";
-
-  return res.status(200).json({
-    message: "Welcome Fantipjs",
-    // metadata: strCompress.repeat(10000),
-  });
-});
+app.use("/", require("./routers"));
 
 //handling error
 
